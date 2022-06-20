dae6b0919620573a58283fa74d8cb918a7426795
fengwei2002 <konng0120@gmail.com>
2022-06-20 17:51:13 +0800

    init vuex

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,24 +1,14 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-    user: {
-      username: "",
-      id: "",
-    }
-  },
-  getters: {
-    fullName(state) {
-      return state.user.firstName + ' ' + state.user.lastName;
-    }
-  },
-  mutations: {
-  },
-  actions: {
-    updateUser(context) {
-      
-    }, 
-  },
-  modules: {
-  }
+	state: {
+	},
+	getters: {
+	},
+	mutations: {
+	},
+	actions: {
+	},
+	modules: {
+	}
 })
